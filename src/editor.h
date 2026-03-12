92d1e334a917c13a32d50e78ca25cbbf8a547942
Jaschin14116120 <jakobschindin@gmx.de>
2026-03-12 21:32:35 +0100

    Update editor.h
    Editor-Modul begonnen: Headerdatei editor.h erstellt

--- src/editor.h
+++ src/editor.h
@@ -1,1 +1,25 @@
+#ifndef EDITOR_H
+#define EDITOR_H
 
+#include <vector>
+#include <cstddef>
+
+/*
+    Setzt ein Byte im Puffer an einer bestimmten Position.
+
+    Parameter:
+      buffer   - Referenz auf den Bytepuffer der geladenen Datei
+      offset   - Position des zu ändernden Bytes
+      newValue - neuer Bytewert
+      oldValue - Rückgabewert: vorheriger Bytewert
+
+    Rückgabe:
+      - true  => Änderung erfolgreich
+      - false => Offset liegt außerhalb des Puffers
+*/
+bool setByteValue(std::vector<unsigned char>& buffer,
+                  std::size_t offset,
+                  unsigned char newValue,
+                  unsigned char& oldValue);
+
+#endif
