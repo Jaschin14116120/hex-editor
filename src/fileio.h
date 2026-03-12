892b9e276a1e7ab56455751a0db8e326b5ce7605
Jaschin14116120 <jakobschindin@gmx.de>
2026-03-12 12:34:23 +0100

    Update fileio.h
    Modul File I/O begonnen: Headerdatei fileio.h erstellt

--- src/fileio.h
+++ src/fileio.h
@@ -1,1 +1,23 @@
+#ifndef FILEIO_H
+#define FILEIO_H
 
+#include <string>
+#include <vector>
+
+/*
+    Laedt eine Datei im Binaermodus in einen Bytepuffer.
+    Rueckgabe:
+      - true  => Datei erfolgreich geladen
+      - false => Fehler beim Oeffnen oder Lesen
+*/
+bool loadFileBinary(const std::string& filename, std::vector<unsigned char>& outBuffer);
+
+/*
+    Speichert einen Bytepuffer im Binaermodus in eine Datei.
+    Rueckgabe:
+      - true  => Datei erfolgreich gespeichert
+      - false => Fehler beim Oeffnen oder Schreiben
+*/
+bool saveFileBinary(const std::string& filename, const std::vector<unsigned char>& buffer);
+
+#endif
